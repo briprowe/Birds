745dd94d5d7cd146784a2caf7e91819b83d9cd1c
Brian Rowe <briprowe@yahoo.com>
2009-09-29 01:24:05 -0400

    Create a "world" to store the birds in, and give each bird a position.

--- Bird.java
+++ Bird.java
@@ -1,11 +1,27 @@
 package sim.app.birds;
 
 import sim.engine.*;
+import sim.util.*;
 
 public class Bird implements Steppable {
 
+    private int id;
+    private Double2D pos;
+
+    public Bird(int i) { 
+	id = i;
+    }
+
+    public void setPos(Double2D p) {
+	pos = p;
+    }
+
+    public Double2D getPos() {
+	return pos;
+    }
+
     public void step(SimState state) {
-	System.out.println("Stepping");
+	System.out.println("Stepping: " + id + "\tPos: (" + pos.getX() + ", " + pos.getY() + ")");
     }
 
 }
--- BirdsModel.java
+++ BirdsModel.java
@@ -1,6 +1,7 @@
 package sim.app.birds;
+
 import sim.engine.*;
-import sim.field.grid.*;
+import sim.field.continuous.*;
 import sim.util.*;
 
 public class BirdsModel extends SimState {
@@ -14,11 +15,34 @@ public class BirdsModel extends SimState {
 
     public BirdsModel(long seed) { super(seed); }
 
+    private Bird[] birds;
+    private Continuous2D bird_grid;
+
+    public void start() {
+	super.start();
+
+	bird_grid = new Continuous2D(100, WORLD_SIZE_X, WORLD_SIZE_Y);
+	birds = new Bird[NUM_BIRDS];
+
+	for( int i = 0; i < NUM_BIRDS; i++ ) {
+	    birds[i] = new Bird(i);
+
+	    Double2D pos = new Double2D(WORLD_SIZE_X * random.nextDouble(),
+					WORLD_SIZE_Y * random.nextDouble());
+	    birds[i].setPos(pos);
+
+	    bird_grid.setObjectLocation(birds[i], birds[i].getPos());
+					
+	}
+
+	schedule.scheduleRepeating(new RandomSequence(birds));
+    }
+
     public static void main(String[] args) {
 
-	System.out.println("Hello!");
+	doLoop(BirdsModel.class, args);
 
-	System.exit(1);
+	System.exit(0);
 
     }
     
