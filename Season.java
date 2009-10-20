2537cfdf43a8ab453e586a76caf2c5d098585d80
Brian Rowe <briprowe@yahoo.com>
2009-10-20 14:16:35 -0400

    Begin to implement the end-of-season mechanics.

--- Season.java
+++ Season.java
@@ -1,13 +1,15 @@
 package sim.app.birds;
 
 import sim.engine.*;
+import sim.field.continuous.*;
+import sim.util.*;
 
 import java.util.*;
 
 public class Season implements Steppable {
     
     private LinkedList<Mating> matings;
-
+    
     public Season() { matings = new LinkedList<Mating>(); }
 
     public void addMating(Mating m) {
@@ -16,5 +18,66 @@ public class Season implements Steppable {
 
     public void step(SimState state) {
 	System.out.println("Season stepping: " + state.schedule.getTimestamp("", "") + " Num Matings: " + matings.size());
+	Continuous2D world = ((BirdsModel)state).getWorld();
+	Bag birds = world.clear();
+
+	// Cull the dead
+	LinkedList<Bird> survivors = cull(birds);
+
+	// Create the list of offspring
+	LinkedList<Bird> young = doBirths();
+
+	// Put the next generation of birds into the scheduler and the world
+	((BirdsModel)state).reschedule(new LinkedList<Bird>(survivors, young));
+	
+	// Create a new list of matings for the next season.
+	matings = new LinkedList<Mating>();
+	
+    }
+
+    private LinkedList<Bird> doBirths() {
+	Iterator iter = matings.iterator();
+	LinkedList<Bird> young = new LinkedList<Bird>();
+
+	try {
+	    Mating m = (Mating)iter.next();
+	    while( m != null ) {
+		// Two offspring per mating 
+		Bird b1 = new Bird();
+		b1.setAge(0);
+		b1.setPos(m.getPosition());
+
+		Bird b2 = new Bird();
+		b2.setAge(0);
+		b2.setPos(m.getPosition());
+		
+		young.add(b1); young.add(b2);
+		
+		Bird mom = m.getFemale();
+		mom.setPregnant(false);
+
+		m = (Mating)iter.next();
+	    }
+	} catch( NoSuchElementException e ) {
+	    
+	}
+
+	return young;
+    }
+    
+    private LinkedList<Bird> cull(Bag birds) {
+	Iterator iter = birds.iterator();
+	LinkedList<Bird> retval = new LinkedList<Bird>();
+
+	try {
+	    Bird b = (Bird)iter.next();
+	    while( b != null ) {
+		retval.add(b);
+
+		b = (Bird)iter.next();
+	    }
+	} catch( NoSuchElementException e ) {
+
+	}
     }
 }
